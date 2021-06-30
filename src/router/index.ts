0619335aabc2ff83b77f1de44d93276bbab29e40
igamasayuki <igamasayuki@gmail.com>
2021-07-01 07:03:34 +0900

    Vue Ruterの設定ファイルを作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,34 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
+  //   path: "/compEmployeeDetail/:id",
+  //   name: "CompEmployeeDetail",
+  //   component: () => import("../components/CompEmployeeDetail.vue"),
   // },
   // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  //   path: "/compEmployeeList",
+  //   name: "CompEmployeeList",
+  //   component: () => import("../components/CompEmployeeList.vue"),
+  // },
+  // {
+  //   path: "/compRegisterAdmin",
+  //   name: "CompRegisterAdmin",
+  //   component: () => import("../components/CompRegisterAdmin.vue"),
+  // },
+  // {
+  //   path: "/compLoginAdmin",
+  //   name: "CompLoginAdmin",
+  //   component: () => import("../components/CompLoginAdmin.vue"),
+  // },
+  // 最終的な受け皿
+  {
+    path: "*",
+    redirect: "/",
+  },
 ]
 
 const router = new VueRouter({
