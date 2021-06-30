431a78d99b5abda10ef4dd23223be2672b2c983e
igamasayuki <igamasayuki@gmail.com>
2021-07-01 05:55:59 +0900

    App.vueにヘッダーコンポーネントとフッターコンポーネントを読み込ませる

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header />
+    <div class="top-wrapper">
+      <router-view />
     </div>
-    <router-view/>
+
+    <Footer />
   </div>
 </template>
 
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  /* width: 800px; */
+  width: 100%;
+  margin: auto;
   color: #2c3e50;
+  /* margin-top: 60px; */
 }
 
-#nav {
-  padding: 30px;
+.top-wrapper {
+  padding-top: 100px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.error {
+  color: red;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
